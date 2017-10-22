515ce4ace7a4b9758bb94bfb1837eb950989d60d
Tim Cash <timcash@gmail.com>
2017-10-22 15:55:31 -0700

    change the constant from ERROR to FAILURE

--- src/failable.js
+++ src/failable.js
@@ -1,7 +1,7 @@
 const equal = require('assert').deepEqual
 
 const SUCCESS = 0
-const ERROR = 1
+const FAILURE = 1
 const EMPTY = 2
 
 const kind = f => f[0]
@@ -9,11 +9,11 @@ const payload = f => f[1]
 const meta = f => f[2]
 
 const success = (payload, meta) => [SUCCESS, payload, meta]
-const failure = (payload, meta) => [ERROR, payload, meta]
+const failure = (payload, meta) => [FAILURE, payload, meta]
 const empty = meta => [EMPTY, undefined, meta]
 
 const isSuccess = f => kind(f) === SUCCESS
-const isFailure = f => kind(f) === ERROR
+const isFailure = f => kind(f) === FAILURE
 const isEmpty = f => kind(f) === EMPTY
 const isFailable = f => isSuccess(f) || isFailure(f) || isEmpty(f)
 
