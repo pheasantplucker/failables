5db3833621394f855f560937f1a801eb0ab1e60f
Tim Cash <timcash@gmail.com>
2017-11-01 14:53:43 -0700

    changing the names of things

--- src/failable.js
+++ src/failable.js
@@ -3,25 +3,25 @@ const equal = require("assert").deepEqual
 const SUCCESS = 0
 const FAILURE = 1
 const EMPTY = 2
-const FAILABLE_TAG_V1 = "fv1"
+const PROTOCOL_V1 = "fv1"
 
-const versionTag = f => f[0]
+const protocolVersion = f => f[0]
 const kind = f => f[1]
 const payload = f => f[2]
 const meta = f => f[3]
 
 const success = (payload, meta) => {
   if (payload === undefined) return empty(meta)
-  return [FAILABLE_TAG_V1, SUCCESS, payload, meta]
+  return [PROTOCOL_V1, SUCCESS, payload, meta]
 }
-const failure = (payload, meta) => [FAILABLE_TAG_V1, FAILURE, payload, meta]
-const empty = meta => [FAILABLE_TAG_V1, EMPTY, undefined, meta]
+const failure = (payload, meta) => [PROTOCOL_V1, FAILURE, payload, meta]
+const empty = meta => [PROTOCOL_V1, EMPTY, undefined, meta]
 
 const isSuccess = f => kind(f) === SUCCESS
 const isFailure = f => kind(f) === FAILURE
 const isEmpty = f => kind(f) === EMPTY
 const isFailable = f => {
-  if (versionTag(f) === FAILABLE_TAG_V1) {
+  if (protocolVersion(f) === PROTOCOL_V1) {
     if (isSuccess(f) || isFailure(f) || isEmpty(f)) {
       return true
     }
