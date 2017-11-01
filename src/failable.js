486718ef4f7e7ab3fd7c543fcbd0368ab9bb118f
Tim Cash <timcash@gmail.com>
2017-11-01 14:51:45 -0700

    adding version to the protocol

--- src/failable.js
+++ src/failable.js
@@ -1,24 +1,33 @@
-const equal = require('assert').deepEqual
+const equal = require("assert").deepEqual
 
 const SUCCESS = 0
 const FAILURE = 1
 const EMPTY = 2
+const FAILABLE_TAG_V1 = "fv1"
 
-const kind = f => f[0]
-const payload = f => f[1]
-const meta = f => f[2]
+const versionTag = f => f[0]
+const kind = f => f[1]
+const payload = f => f[2]
+const meta = f => f[3]
 
 const success = (payload, meta) => {
   if (payload === undefined) return empty(meta)
-  return [SUCCESS, payload, meta]
+  return [FAILABLE_TAG_V1, SUCCESS, payload, meta]
 }
-const failure = (payload, meta) => [FAILURE, payload, meta]
-const empty = meta => [EMPTY, undefined, meta]
+const failure = (payload, meta) => [FAILABLE_TAG_V1, FAILURE, payload, meta]
+const empty = meta => [FAILABLE_TAG_V1, EMPTY, undefined, meta]
 
 const isSuccess = f => kind(f) === SUCCESS
 const isFailure = f => kind(f) === FAILURE
 const isEmpty = f => kind(f) === EMPTY
-const isFailable = f => isSuccess(f) || isFailure(f) || isEmpty(f)
+const isFailable = f => {
+  if (versionTag(f) === FAILABLE_TAG_V1) {
+    if (isSuccess(f) || isFailure(f) || isEmpty(f)) {
+      return true
+    }
+  }
+  return false
+}
 
 const anyFailed = l => l.filter(isFailure).length > 0
 const firstFailure = l => l.filter(isFailure)[0]
@@ -50,23 +59,28 @@ const makeItFailable = fn => {
 const clean = o => {
   return Object.keys(o)
     .filter(f => o[f] !== undefined)
-    .reduce((r, i) => Object.assign(r, {[i]: o[i]}), {})
+    .reduce((r, i) => Object.assign(r, { [i]: o[i] }), {})
 }
 
 const kindString = f => {
   switch (kind(f)) {
-    case SUCCESS: return 'success'
-    case FAILURE: return 'failure'
-    case EMPTY: return 'empty'
-    default: return 'unknown'
+    case SUCCESS:
+      return "success"
+    case FAILURE:
+      return "failure"
+    case EMPTY:
+      return "empty"
+    default:
+      return "unknown"
   }
 }
 
-const hydrate = f => clean({
-  kind: kindString(f),
-  payload: payload(f),
-  meta: meta(f)
-})
+const hydrate = f =>
+  clean({
+    kind: kindString(f),
+    payload: payload(f),
+    meta: meta(f)
+  })
 
 module.exports = {
   kind,
